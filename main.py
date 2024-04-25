8e8905523e397dd221aca37796fc1672c32e2d71
Chiarasenu <pererachiara2@gmail.com>
2024-04-25 17:42:14 +0200

    schermo

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame, sys
+from pygame.locals import *
+
+
+#schermo
+
+window = (1000, 650)
+screen = pygame.display.set_mode(window)
+pygame.display.set_caption('gioco')
+screen.fill((2, 247, 178))
+
+
+
+
+
+running = True
+
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == pygame.KEYDOWN: 
+            run = True
+
+        
+    pygame.display.update()
+    
+pygame.quit()            
+    
