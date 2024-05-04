26a1935b794636bc88eca8c8fa3c344aa46e0feb
Chiarasenu <pererachiara2@gmail.com>
2024-05-04 18:02:06 +0200

    commento inutile

--- main.py
+++ main.py
@@ -8,22 +8,21 @@ window = (1000, 650)
 screen = pygame.display.set_mode(window)
 pygame.display.set_caption('gioco')
 screen.fill((2, 247, 178))
-
 running = True
-
 clock = pygame.time.Clock()
 fps = 60
-
 while running:
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         elif event.type == pygame.KEYDOWN: 
             run = True   
-
     pygame.display.update()
     clock.tick(fps)
-    
-pygame.quit()            
-    
+pygame.quit() 
+
+#eventuali personaggi
+
+  
+
+
