2cd1dfa4dad07b841891d5a79fb3f45d7ce7f3ff
Chiarasenu <pererachiara2@gmail.com>
2024-05-08 13:15:59 +0200

    boh

--- gioco/immagini/main.py
+++ gioco/immagini/main.py
@@ -5,8 +5,8 @@ pygame.init()
 clock = pygame.time.Clock()
 FPS = 60
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 375
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 1700
 
 #schermo
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
