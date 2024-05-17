a201a52e03e4d56f60bb6993e9ed57686c926773
Chiarasenu <pererachiara2@gmail.com>
2024-05-17 16:53:35 +0200

    eliminazione pygame draw

--- gioco/Oggetti/snake.py
+++ gioco/Oggetti/snake.py
@@ -2,7 +2,8 @@ from math import Vector2
 
 class Snake:
     def __init__(self) -> None:
-        self.corpo = [Vector2]
+        # self.corpo = [Vector2(x,y),Vector2(x,y),Vector2(x,y)]
+        
 
 # Serve:
 # - Coda, Corpo e Testa
