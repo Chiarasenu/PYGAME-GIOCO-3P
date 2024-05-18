6d10b800c83a4c7fb473653a8003b1d8252cb9a4
maxnobili <massimiliano.nobili.07@gmail.com>
2024-05-18 19:20:25 +0200

    ....

--- gioco/Oggetti/snake_blu.py
+++ gioco/Oggetti/snake_blu.py
@@ -7,9 +7,31 @@ class Snake_Blu:
         self.direzione = Vector2(0,0)
         self.nuovo_corpo = False
 
-        # IMMAGINI BLU
-            # Testa
+    # IMMAGINI
+        # Testa
         self.testaN = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_testaN.png')
+        self.testaE = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_testaE.png')
+        self.testaS = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_testaS.png')
+        self.testaO = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_testaO.png')
+
+        # Corpo
+        self.corpoO = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_corpo_orizzontale.png')
+        self.corpoV = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_corpo_verticale.png')
+
+        # Coda
+        self.codaN = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_codaN.png')
+        self.codaE = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_codaE.png')
+        self.codaS = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_codaS.png')
+        self.codaO = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_codaO.png')
+
+        # Angoli
+        self.angoloNE = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_angoloNE.png')
+        self.angoloNO = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_angoloNO.png')
+        self.angoloSE = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_angoloSE.png')
+        self.angoloSO = pygame.image.load('Gioco/Immagini/PartiSnake/Blu/snake_blu_angoloSO.png')
+
+    
+
 
 # Serve:
 # - Coda, Corpo e Testa
