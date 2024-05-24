d7f4f96c46b3c19dd26090b17182fb547ec68f24
maxnobili <massimiliano.nobili.07@gmail.com>
2024-05-24 09:02:27 +0200

    creazione classe mela

--- gioco/Oggetti/Frutta/frutta.py
+++ gioco/Oggetti/Frutta/frutta.py
@@ -1,7 +1,18 @@
-class Mela:
-    def __init__(self) -> None:
-        pass
+import pygame
+from pygame.math import Vector2
+from random import randint
 
+mela = pygame.image.load('Gioco/Immagini/Frutta/mela.png').convert_alpha()
+
+class Mela:
+    def __init__(self, numero_celle) -> None:
+        self.x = randint(0, numero_celle -1)
+        self.y = randint(0, numero_celle -1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_mela(self, lato_celle, screen):
+        mela_rect = pygame.Rect(int(self.pos.x * lato_celle), int(self.pos.y * lato_celle))
+        screen.blit(mela, mela_rect)
 # Serve 
 # 1. la randomizzazione della posizione
 # 2. il rect
