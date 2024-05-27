42fdd60ce89358c01f77ed665079a836d406c6c0
maxnobili <massimiliano.nobili.07@gmail.com>
2024-05-27 19:25:30 +0200

    2

---
+++ gioco/commenti.py
@@ -0,0 +1,11 @@
+# from enum import Enum
+ 
+# class enum_frutta(Enum):
+#     MELA = 'Gioco/Immagini/Frutta/mela.png'
+#     CILIEGIA = 'Gioco/Immagini/Frutta/mela.png'
+#     LIMONE = 'Gioco/Immagini/Frutta/mela.png'
+#     ANGURIA = 'Gioco/Immagini/Frutta/mela.png'
+
+# ogg_frutta = frutta(screen, [randint(0, numero_celle -1), 
+#  randint(0, numero_celle -1)], lato_celle, enum_frutta.MELA.name,
+#  enum_frutta.MELA.value)
